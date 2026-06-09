c97946985cfb7eb7d33179ebdc95269653808085
Sayan Kumar Roy <sayankumarr02@gmail.com>
2026-06-10 02:15:01 +0530

    i don't know bro finger out you self

--- render_start.sh
+++ render_start.sh
@@ -3,5 +3,4 @@ set -o errexit
 
 cd AUdoc_back
 python manage.py migrate --noinput
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='sayan').exists() or User.objects.create_superuser('sayan', 'admin@example.com', 'sayan')"
 gunicorn AUdoc_back.wsgi:application --bind 0.0.0.0:$PORT
