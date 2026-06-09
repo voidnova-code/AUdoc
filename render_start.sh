90d6f3a7687e5707db7e0ca59109e1b77cd36161
Sayan Kumar <sayankumarr02@gmail.com>
2026-06-10 02:10:33 +0530

    Update render_start.sh to create superuser sayan

--- render_start.sh
+++ render_start.sh
@@ -1,6 +1,7 @@
-﻿#!/usr/bin/env bash
+#!/usr/bin/env bash
 set -o errexit
 
 cd AUdoc_back
 python manage.py migrate --noinput
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='sayan').exists() or User.objects.create_superuser('sayan', 'admin@example.com', 'sayan')"
 gunicorn AUdoc_back.wsgi:application --bind 0.0.0.0:$PORT
