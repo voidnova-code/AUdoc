115a2f826e6285c375a508326112a282ca8f30b6
Sayan Kumar <sayankumarr02@gmail.com>
2026-06-10 01:12:04 +0530

    Add render build and start scripts

---
+++ render_build.sh
@@ -0,0 +1,6 @@
+﻿#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+cd AUdoc_back
+python manage.py collectstatic --noinput
---
+++ render_start.sh
@@ -0,0 +1,6 @@
+﻿#!/usr/bin/env bash
+set -o errexit
+
+cd AUdoc_back
+python manage.py migrate --noinput
+gunicorn AUdoc_back.wsgi:application --bind 0.0.0.0:$PORT
