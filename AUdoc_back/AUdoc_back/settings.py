0a882788149aac3a745855385ea94d6ce0e4ea79
Sayan Kumar <sayankumarr02@gmail.com>
2026-09-15 22:11:40 +0530

    fix: add WhiteNoise for static file serving in production

--- AUdoc_back/AUdoc_back/settings.py
+++ AUdoc_back/AUdoc_back/settings.py
@@ -102,6 +102,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "csp.middleware.CSPMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -221,8 +222,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL  = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
